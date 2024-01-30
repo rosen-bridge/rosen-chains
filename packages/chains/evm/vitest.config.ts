293a4ef57fb4b5dcb408f7b7cafd2a50fa681340
radni <amirhossein_kh@ymail.com>
2024-01-30 12:06:48 +0330

    vitest config bug fix

--- packages/chains/evm/vitest.config.ts
+++ packages/chains/evm/vitest.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     coverage: {
       all: true,
       reporter: ['cobertura', 'text', 'text-summary'],
+      provider: 'v8',
     },
     passWithNoTests: true,
-    threads: false,
   },
 });
