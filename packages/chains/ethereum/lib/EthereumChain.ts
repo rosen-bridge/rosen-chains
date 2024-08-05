dc084d5bd0b671f324c33f5ce078dffb15b5d0bb
HFazelinia <hadifazelinia78@gmail.com>
2024-08-05 09:35:00 +0100

    remove EthereumChain constructor

--- packages/chains/ethereum/lib/EthereumChain.ts
+++ packages/chains/ethereum/lib/EthereumChain.ts
@@ -10,16 +10,6 @@ class EthereumChain extends EvmChain {
   CHAIN = 'ethereum';
   NATIVE_TOKEN_ID = 'eth';
   CHAIN_ID = 1n;
-
-  constructor(
-    network: AbstractEvmNetwork,
-    configs: EvmConfigs,
-    tokens: RosenTokens,
-    supportedTokens: Array<string>,
-    signFunction: TssSignFunction
-  ) {
-    super(network, configs, tokens, supportedTokens, signFunction);
-  }
 }
 
 export default EthereumChain;
